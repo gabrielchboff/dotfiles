e3f30bbad34679837fe42f2d4c8fd42912eb1909
gabrielchboff <gabrielchagasboff@gmail.com>
2023-08-20 13:38:45 -0300

    feat: new nvim config, bashrc addons and new apps

--- installers/arch_linux/install_apps.sh
+++ installers/arch_linux/install_apps.sh
@@ -1,22 +1,17 @@
-sudo pacman -Sy opera firefox docker docker-compose wezterm helix flatpak ufw \
-  libreoffice-fresh vlc neofetch reflector gimp krita kdenlive 
+sudo pacman -Sy firefox docker docker-compose konsole helix flatpak ufw \
+	libreoffice-fresh vlc neofetch reflector gimp krita kdenlive tmux dolphin kdewallet kdeconnect
 
 sudo reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-yay -Sy zoom discord spotify dbeaver steam preload timeshift
+yay -Sy zoom discord spotify dbeaver steam preload timeshift teams-for-linux brave-bin
 
-git clone https://aur.archlinux.org/snapd.git
-cd snapd
-makepkg -si
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
-cd ~
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub com.visualstudio.code
+flatpak install flathub com.obsproject.Studio
 
 sudo pacman -Sy
 sudo ufw enable
 sudo ufw status verbose
 sudo systemctl enable ufw.service
 sudo systemctl enable preload && sudo systemctl start preload
-sudo snap install todoist
-
